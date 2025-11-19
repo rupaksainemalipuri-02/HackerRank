165d97f10cb3ccd296fba4f6ba8d0e52275969c1
Rupak Sai Nemalipuri <rupaksainemalipuri@gmail.com>
2025-11-19 12:59:35 +0530

    Add comments to helloworld.c
    Add comments for input/output format and constraints

--- helloworld.c
+++ helloworld.c
@@ -1,3 +1,17 @@
+/*Write a C program to print the message: Hello World!
+
+Input Format
+
+No input is required.
+
+Constraints
+
+N/A
+
+Output Format
+
+Single line printing exactly: Hello World!*/
+
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
