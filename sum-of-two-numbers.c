353f407acab71aeb72ad5267098955850e1c667f
Rupak Sai Nemalipuri <rupaksainemalipuri@gmail.com>
2025-11-19 12:58:21 +0530

    Update sum-of-two-numbers.c

--- sum-of-two-numbers.c
+++ sum-of-two-numbers.c
@@ -1,3 +1,17 @@
+/*Write a program that takes two integers as input and prints their sum.
+
+Input Format
+
+Two unique integers a and b.
+
+Constraints
+
+-10^4 ≤ a, b ≤ 10^4
+Inputs will always be valid integers
+Output Format
+
+Single line printing the result as: The sum of {a} and {b} is {sum}.*/
+
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
