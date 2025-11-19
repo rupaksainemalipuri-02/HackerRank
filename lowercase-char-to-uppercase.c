f50686d3c01ce4471760ba413046222291075a9f
Rupak Sai Nemalipuri <rupaksainemalipuri@gmail.com>
2025-11-19 13:00:19 +0530

    Update lowercase-char-to-uppercase.c

--- lowercase-char-to-uppercase.c
+++ lowercase-char-to-uppercase.c
@@ -1,3 +1,22 @@
+/*Write a C program that takes a single lowercase English letter as input and prints its corresponding uppercase letter.
+
+Use the ASCII value method to perform the conversion.
+
+Input Format
+
+A single lowercase character charLower.
+
+Constraints
+
+Input must be a lowercase English alphabet ('a' to 'z').
+Do not use built-in string or character functions like toupper() or strupr().
+Conversion must be done using ASCII value arithmetic.
+Output Format
+
+A single line in the format:
+
+The uppercase of {charLower} is {charUpper}*/
+
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
