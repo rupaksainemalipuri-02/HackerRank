42140998d1b8a603360454d9c3bbc74517c3e5a2
Rupak Sai Nemalipuri <rupaksainemalipuri@gmail.com>
2025-12-16 23:38:02 +0530

    Create print-n-natural-numbers.c

---
+++ print-n-natural-numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n, i;
+    scanf("%d", &n);
+    for (i=1; i<=n; ++i){
+        printf("%d ", i);
+    }
+    return 0;
+}
